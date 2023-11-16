dc03830bc974539a75003ca55b0ded98339613f2
Benjamin <86573978+bennie10colado@users.noreply.github.com>
2023-11-16 11:40:57 -0300

    basic structure of server.js

---
+++ src/backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
